1a9329eb6547633ef1c4987e04a5ab7c205400af
tndrmanjvbox <a01654827@itesm.mx>
2020-05-02 22:26:33 -0500

    .

--- NFA2DFA.py
+++ NFA2DFA.py
@@ -1,5 +1,6 @@
 import readNFA
 import NFA2DFAVIVS
+
 class transicion:
     def __init__(self, lectura,estado, destino):
         self.estado = estado
@@ -10,6 +11,75 @@ class transicion:
         return (self.lectura, self.estado, self.destino)
     def miEstado(self):
         return self.estado
+
+
+
+
+def dfaGen(arrTransiciones):
+    copyTrans = arrTransiciones.copy()
+    arrParaNetworkX = []
+    for i in arrTransiciones:
+        if (i.estado in arrParaNetworkX):
+            pass
+        else:
+            arrParaNetworkX.append(i.estado)
+        if (i.destino in arrParaNetworkX):
+            pass
+        else:
+            arrParaNetworkX.append(i.destino)
+    for estado in arrParaNetworkX:
+        #print("ESTADO:    ", estado)
+        newState0 = []
+        newState1 = []
+        for x in arrTransiciones:
+            if ((x.estado) == estado):
+                if (x.lectura == '0'):
+                    newState0.append(x.destino)
+                    if (len(newState0) > 1):
+                        arrParaNetworkX.append(newState0)
+                if (x.lectura == '1'):
+                    newState1.append(x.destino)
+                    if (len(newState1) > 1):
+                        arrParaNetworkX.append(newState1)
+    newaa = []
+    for x in arrParaNetworkX:
+        if (len(x)> 1):
+            for y in arrParaNetworkX:
+                if y in x:
+                    pass
+                elif y == x:
+                    pass
+                else:
+                    newEs = x.copy()
+                    newEs.append(y)
+                    newaa.append(newEs)
+    for x in newaa:
+        arrParaNetworkX.append(x)
+    for j in arrParaNetworkX:
+        if (len(j) > 1):
+            for n in arrTransiciones:
+                if (n.destino in j):
+                    # print(n.miTrans())
+                    n.destino = j
+                if (n.estado in j):
+                    # print(n.miTrans())
+                    if (j[0] == n.estado):
+                        pass
+                    else:
+                        n.estado = j
+                    pass
+    print(arrParaNetworkX)
+
+
+    for x in copyTrans:
+        print(x.miTrans())
+
+    print("//////////////////////")
+
+    for n in arrTransiciones:
+        print(n.miTrans())
+
+
 
 def recorrerEstados(arrTransiciones):
     arrTranOriginal = arrTransiciones
@@ -65,7 +135,7 @@ def recorrerEstados(arrTransiciones):
         print(n.miTrans())
 
 def vivMain():
-    data = NFA2DFAVIVS.readTXT('newNFA.txt')
+    data = NFA2DFAVIVS.readTXT('NFA.txt')
     newStates = NFA2DFAVIVS.generateStates(data)
     newTranStates = NFA2DFAVIVS.newTansitionsStates(data)
     DFA = NFA2DFAVIVS.newTransition(newStates, newTranStates)
@@ -75,10 +145,10 @@ def vivMain():
 def miMain():
     print("NFA 2 DFA PYTHON SCRIPT")
     arrTransiciones = []
-    txt = readNFA.readTXT("NFA.txt")
+    txt = readNFA.readTXT("newNFA.txt")
     for tr in txt:
         arrTransiciones.append(transicion(tr[0], tr[1], tr[2]))
-    recorrerEstados(arrTransiciones)
+    dfaGen(arrTransiciones)
 
 if __name__ == '__main__':
     miMain()
